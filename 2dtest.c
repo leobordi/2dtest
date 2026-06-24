be2fbcd497080ba892348a8011793ae0c1096080
leobordi <leobordignon849@gmail.com>
2026-06-24 21:36:01 +0200

    Add Makefile and change draw logic

--- 2dtest.c
+++ 2dtest.c
@@ -1,20 +1,22 @@
 #include <SDL2/SDL.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdint.h>
 
-#define HEIGHT 900 
-#define WIDTH 1500
+#define NUMPOINTS 10000
 
-void buffer_clear(int *buffer) {
-    for (int i = 0; i < HEIGHT*WIDTH; i++) {
-        buffer[i] = 0;
-    }
+void clear(SDL_Surface *surface) {
+    memset((uint8_t *)surface->pixels, 0, surface->h * surface->pitch);
 }
 
-void draw(int *buffer, int *points, int num) {
-    for (int i = 0; i < num; i++) {
-        for (int k = points[i]; k < points[i+1]-points[i]; k++) {
-            buffer[k] = 1;
+void draw(SDL_Surface *surface) {
+    int width = surface->w;
+    int height = surface->h; 
+
+    for (int x = 0; x < width; x++) {
+        for (int y = 0; y < height; y++) {
+            uint8_t *fb = (uint8_t*)surface->pixels;
+            fb[x + y] = 1;
         }
     }
 }
@@ -25,35 +27,23 @@ int main() {
         exit(-1);
     }
     
-    SDL_Window *window = SDL_CreateWindow(
-        "2D test",
-        SDL_WINDOWPOS_CENTERED, 
-        SDL_WINDOWPOS_CENTERED,
-        1500,
-        900,
-        SDL_WINDOW_SHOWN 
-    ); 
-
-    if (window == NULL) {
+    SDL_Window *window = SDL_CreateWindow("2D test", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1500, 900, SDL_WINDOW_SHOWN); 
+    if (!window) {
         printf("Errore di creazione finestra!\n");
         exit(-1);
     } 
  
-    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    SDL_Texture *texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STREAMING, WIDTH, HEIGHT);
-    
-    int buffer[HEIGHT*WIDTH];
-    int point_ori = HEIGHT/2 * WIDTH/2;
-    
-    buffer_clear(buffer);
-    int points[2] = {0, 200};
+    SDL_Surface* surface = SDL_GetWindowSurface(window);
+    if (!surface) {
+        SDL_Log("SDL_GetWindowSurface failed: %s", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+    clear(surface);
 
-    draw(buffer, points, 2);
-    SDL_UpdateTexture(texture, NULL, buffer, 64 * sizeof(int));
-    SDL_RenderClear(renderer);
-    SDL_RenderCopy(renderer, texture, NULL, NULL);
-    SDL_RenderPresent(renderer);
-    
+    draw(surface);
+
     while (1) {    
         SDL_Event event;
         bool exit = false;
--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 all: 2dtest
 
 2dtest: 2dtest.c
-	gcc 2dtest.c -O2 -Wall $(shell sdl2-config --cflags --libs) -o 2dtest
+	gcc 2dtest.c -O2 -Wall $(shell sdl2-config --cflags --libs) -o ./build/2dtest
 
 clean:
 	rm -f 2dtest
